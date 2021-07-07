37c28b54772f2c122cff1831282e9cb1c27fa54f
VladBreaban <vlad.breaban@relevance.ro>
2021-07-07 17:15:49 +0000

    templates

---
+++ PersonalTimePlanning/StaticHelperFunctions/Functions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalTimePlanning.StaticHelperFunctions
+{
+    public static partial class Functions
+    {
+
+    }
+}

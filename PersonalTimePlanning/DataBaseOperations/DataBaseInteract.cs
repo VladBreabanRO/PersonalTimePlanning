85bb0dbc482346b8ac991ff25f66e154335a18c1
VladBreaban <vlad.breaban@relevance.ro>
2021-07-10 17:34:59 +0000

    functie db interact schitat

---
+++ PersonalTimePlanning/DataBaseOperations/DataBaseInteract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalTimePlanning.DataBaseOperations
+{
+    public class DataBaseInteract
+    {
+        private string _connectionString = @"Data Source = {0}\SQLEXPRESS;Initial Catalog = {1}; Integrated Security = SSPI";
+        private static readonly string SERVER_NAME = Environment.MachineName;
+        private static string DATABASE_NAME = "PlannerApp";
+
+        public  DataBaseInteract()
+        {
+            this._connectionString = string.Format(this._connectionString, SERVER_NAME, DATABASE_NAME);
+        }
+
+        // to do: functii de genul getTskList
+        //exemplu mai jos:
+        public async Task getTaskList()
+        {
+            // scris cod, folosint connection string-ul de mai sus,, pt a luat lista task-urile din db
+        }
+
+    }
+}
